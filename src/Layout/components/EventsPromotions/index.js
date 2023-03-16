656200080fe6466e94577b7aeb0b725c034d237c
SongLe <lesong0162@gmail.com>
2023-03-16 20:28:29 +0700

    update

--- src/Layout/components/EventsPromotions/index.js
+++ src/Layout/components/EventsPromotions/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import './EventsPromotions.scss';
 import InfiniteCarousel from 'react-leaf-carousel';
+import 'react-circular-progressbar/dist/styles.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
 import GangstarVegasJessie from '../../../assets/img/GangstarVegasJessie.png';
 import events_promotions1 from '../../../assets/img/events_promotions1.png';
 import events_promotions2 from '../../../assets/img/events_promotions2.png';
 import events_promotions3 from '../../../assets/img/events_promotions3.png';
 import events_promotions4 from '../../../assets/img/events_promotions4.jpg';
 import Share from '../../../assets/img/share.png';
+import './EventsPromotions.scss';
 function EventsPromotions() {
   const sliderItem = [
     { img: GangstarVegasJessie, link: '#' },
@@ -42,19 +44,17 @@ function EventsPromotions() {
             },
           },
         ]}
-        // animationDuration={1000}
+        animationDuration={1000}
         dots={true}
-        paging={true}
-        // pagingSeparator
         swipe={false}
         showSides={true}
         slidesToScroll={4}
         slidesToShow={4}
         sideSize={1}
-        // scrollOnDevice={true}
+        scrollOnDevice={true}
         autoCycle={true}
         className="WrapperSlider"
-        // cycleInterval={3000}
+        cycleInterval={3000}
         slidesSpacing={5}
       >
         {sliderItem.map((item, index) => (
@@ -80,6 +80,20 @@ function EventsPromotions() {
           </div>
         ))}
       </InfiniteCarousel>
+      {/* <div className="Circular">
+        <div style={{ width: '20px', height: '20px' }}>
+          <CircularProgressbar value={0.66} maxValue={1} text={`${0.66 * 100}%`} />
+        </div>
+        <div style={{ width: '20px', height: '20px' }}>
+          <CircularProgressbar value={0.66} maxValue={1} text={`${0.66 * 100}%`} />
+        </div>
+        <div style={{ width: '20px', height: '20px' }}>
+          <CircularProgressbar value={0.66} maxValue={1} text={`${0.66 * 100}%`} />
+        </div>
+        <div style={{ width: '20px', height: '20px' }}>
+          <CircularProgressbar value={0.66} maxValue={1} text={`${0.66 * 100}%`} />
+        </div>
+      </div> */}
     </div>
   );
 }
