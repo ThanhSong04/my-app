b20ae8eaefd60c1cabc8dd272b26964592cec171
SongLe <lesong0162@gmail.com>
2023-03-16 13:01:59 +0700

    update full scoll screen desktop

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -4,13 +4,16 @@ import './Menu.scss';
 const Menu = ({ cards }) => (
   <nav id="menu">
     <ul>
-      {Array.from({ length: 4 }, (_, i) => i + 1).map((card) => (
-        <li key={card}>
-          <ScrollTo selector={`#card${card}`}>
-            <button></button>
-          </ScrollTo>
-        </li>
-      ))}
+      {Array.from({ length: 4 }, (_, i) => i + 1).map((card) => {
+        console.log(card);
+        return (
+          <li key={card}>
+            <ScrollTo selector={`#card${card}`}>
+              <button></button>
+            </ScrollTo>
+          </li>
+        );
+      })}
     </ul>
   </nav>
 );
